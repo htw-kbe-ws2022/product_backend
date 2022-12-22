281018a8964c910b96e75ed0fbd8c399a7a24b16
JohannesPlath <plath.johannes@gmail.com>
2022-12-22 17:36:46 +0100

    db reset bearbeitet

--- src/main/java/com/htwkbews2022/product_backend/ProductBackendApplication.java
+++ src/main/java/com/htwkbews2022/product_backend/ProductBackendApplication.java
@@ -62,7 +62,7 @@ public class ProductBackendApplication {
 					.category(Category.DOPE)
 					.price(12.50d)
 					.description("high intensive dark smoked weed")
-					.picUrl("https://cdn.pixabay.com/photo/2019/09/21/15/40/marijuana-4494190_960_720.jpg")
+					.picUrl("http://localhost:8081/pic/bobble")
 					.count(4)
 					.build());
 			foodRepository.save(Product
@@ -73,7 +73,7 @@ public class ProductBackendApplication {
 					.price(9.50d)
 					.count(4)
 					.description("Fancy visionary light dope")
-					.picUrl("https://cdn.pixabay.com/photo/2019/09/21/15/40/marijuana-4494190_960_720.jpg")
+					.picUrl("http://localhost:8081/pic/cannabis-gb5c924ecf_1920")
 					.build());
 			foodRepository.save(Product
 					.builder()
@@ -82,7 +82,7 @@ public class ProductBackendApplication {
 					.category(Category.DOPE)
 					.price(11.50d)
 					.description("classic purple dope")
-					.picUrl("https://cdn.pixabay.com/photo/2019/09/21/15/40/marijuana-4494190_960_720.jpg")
+					.picUrl("http://localhost:8081/pic/purple-hemp-flowers-medical-cannabis")
 					.count(4)
 					.build());
 			foodRepository.save(Product
@@ -92,7 +92,7 @@ public class ProductBackendApplication {
 					.category(Category.PROCESSED)
 					.price(2.50d)
 					.description("Sweet lolipopp with 1g thc")
-					.picUrl("https://cdn.pixabay.com/photo/2019/09/21/15/40/marijuana-4494190_960_720.jpg")
+					.picUrl("http://localhost:8081/pic/justin-aikin-5Dyrcn5ocfk-unsplash.jpg")
 					.count(2)
 					.build());
 			foodRepository.save(Product
@@ -101,7 +101,7 @@ public class ProductBackendApplication {
 					.currency(Currency.EURO)
 					.category(Category.DOPE)
 					.description("the classic dope")
-					.picUrl("https://cdn.pixabay.com/photo/2019/09/21/15/40/marijuana-4494190_960_720.jpg")
+					.picUrl("http://localhost:8081/pic/cambridge-jenkins-iv-KJbK6WqpGd4-unsplash.jpg")
 					.price(13.50d)
 					.count(7)
 					.build());
@@ -113,6 +113,26 @@ public class ProductBackendApplication {
 					.price(3.50d)
 					.description("a cup, a f*** cup")
 					.picUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIXS93Vb0IKHbrv4qGhLV4S0m637GXzaAXieRiAePatOhjl9G_1xjHjy0gCUiUFrUwEEo&usqp=CAU")
+					.count(7)
+					.build());
+			foodRepository.save(Product
+					.builder()
+					.title("White Widow")
+					.currency(Currency.EURO)
+					.category(Category.DOPE)
+					.price(10.73d)
+					.description("classic fine turned Dope with a fresh blend")
+					.picUrl("http://localhost:8081/pic/whiteWidow.jpg")
+					.count(7)
+					.build());
+			foodRepository.save(Product
+					.builder()
+					.title("Roled One")
+					.currency(Currency.EURO)
+					.category(Category.PROCESSED)
+					.price(6.25d)
+					.description("blended sativa Sticky")
+					.picUrl("http://localhost:8081/pic/rolled.jpg")
 					.count(7)
 					.build());
 		};
